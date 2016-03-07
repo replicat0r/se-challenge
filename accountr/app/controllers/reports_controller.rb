e6c6a714a0aedef93d29ba91a2a64d28f19b6a33
tyler liu <replicat0r.misc@gmail.com>
2016-03-06 20:10:14 -0500

    setup controller and model skeleton methods

--- accountr/app/controllers/reports_controller.rb
+++ accountr/app/controllers/reports_controller.rb
@@ -1,10 +1,18 @@
 class ReportsController < ApplicationController
   def create
+  	Report.process_file(file_param)
+  	redirect_to reports_path
   end
 
   def index
+  	@report = Report.new
   end
 
   def new
   end
+
+  private 
+ 		def file_param
+ 			params.require(:report).permit(:file)
+ 		end
 end
--- accountr/app/models/report.rb
+++ accountr/app/models/report.rb
@@ -1,2 +1,12 @@
 class Report < ActiveRecord::Base
+
+	def self.process_file(file)
+		#process csv and import it into the db
+	
+	end
+
+	def self.open_file(file)
+		#parse csv file and return the object
+
+	end
 end
