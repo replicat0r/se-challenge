ef4c2d9ef0e2a8545bfeaa5fe4f0fcb1afc22275
tyler liu <replicat0r.misc@gmail.com>
2016-03-09 19:01:38 -0500

    updated the readme and initilizer

--- accountr/config/initializers/assets.rb
+++ accountr/config/initializers/assets.rb
@@ -8,25 +8,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( app.js materialize.scss report.scss )
 # }
 
-Rails.application.config.assets.precompile << Proc.new { |path|
-  if path =~ /\.(scss|js|png|svg|eot|woff|ttf)\z/
-    full_path = Rails.application.assets.resolve(path)
-    app_assets_path = Rails.root.join('app', 'assets').to_s
-    #vendor_assets_path = Rails.root.join('vendor', 'assets','bower_components','unitegallery').to_s
-
-    #if ((full_path.starts_with? app_assets_path) || (full_path.starts_with? vendor_assets_path)) && (!path.starts_with? '_')
-    if (full_path.starts_with? app_assets_path) && (!path.starts_with? '_')
-      puts "including asset: " + full_path
-
-      #puts "\t" + full_path.slice(Rails.root.size..-1)
-      true
-    else
-      false
-    end
-  else
-    false
-  end
-}
